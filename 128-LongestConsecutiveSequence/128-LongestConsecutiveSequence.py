c04cedd11a5196ac70fb3cbd50004fd6b0517121
Saurabh Thakur <63082305+thakursc1@users.noreply.github.com>
2025-08-03 08:38:23 -0700

    Create 128-LongestConsecutiveSequence

---
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
@@ -0,0 +1,23 @@
+# Last updated: 03/08/2025, 08:38:23
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        num_set = set(nums)
+        longest_streak = 0
+
+
+        for num in num_set:
+            if num-1 not in num_set:
+                current_num = num
+                current_streak = 1 
+
+
+                while current_num+1 in num_set:
+                    current_num+=1
+                    current_streak+=1
+                
+                longest_streak = max(longest_streak, current_streak)
+        
+
+        return longest_streak
+            
